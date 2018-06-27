be457351ad28e90011400009b9b67e94f1045f9b
treptow <treptow@apollointeractive.com>
2018-06-26 17:58:53 -0700

    misc edits

--- contexts/dashboard/controller.js
+++ contexts/dashboard/controller.js
@@ -1,11 +1,21 @@
 const Queries = require('../queries/model.js');
 
-exports.load = (req, res) => {
+exports.index = (req, res) => {
   Queries.
     find( {user: req.session.userId} ).
     exec(function (err, queries) {
       if (err) return handleError(err);
       //map out unrelated user information
       return res.render('dashboard', { queries: queries, authed: true });
+  });
+}
+
+exports.single = (req, res) => {
+  Queries.
+    find( {user: req.session.userId} ).
+    exec(function (err, queries) {
+      if (err) return handleError(err);
+      //map out unrelated user information
+      return res.render('dashboard-single', { queries: queries, authed: true });
   });
 }
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -6,7 +6,18 @@ const controller = require('../contexts/dashboard/controller.js');
 router.get('/', function(req, res, next) {
   if(req.session.userId){
     // load in dashboard data
-    controller.load(req, res)
+    controller.index(req, res)
+  } else {
+    //Not logged in
+    return res.redirect('users/login');
+  }
+});
+
+/* Check to see if logged in */
+router.get('/:postID', function(req, res, next) {
+  if(req.session.userId){
+    // load in dashboard data
+    controller.single(req, res)
   } else {
     //Not logged in
     return res.redirect('users/login');

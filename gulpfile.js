9cb28304c99a0485f12e2e057e595ea4bdbb1de5
csliva <colt.sliva@gmail.com>
2018-07-06 15:49:30 -0400

    homepage edits

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 //gulpfile.js
-
 var gulp = require('gulp');
 var watch = require('gulp-watch')
 var sass = require('gulp-sass');
@@ -8,12 +7,27 @@ var sass = require('gulp-sass');
 var sassFiles = 'public/sass/**/*.sass',
     cssDest = 'public/stylesheets';
 
-gulp.task('styles', function(){
-    gulp.src(sassFiles)
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest(cssDest));
+let compileSass = function(){
+  gulp.src(sassFiles)
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest(cssDest));
+}
+
+gulp.task('styles', function(done){
+  compileSass()
+  done();
 });
 
-gulp.task('watch',function() {
-    return watch(sassFiles, ['styles']);
+gulp.task('watch', function(done){
+    gulp.watch(sassFiles)
+    .on('change', function(path, stats) {
+        console.log(path);
+        compileSass();
+        done();
+    })
+    .on('unlink', function(path, stats) {
+        console.log(path);
+        // code to execute on delete
+    });
 });
+gulp.task('default', gulp.series(['styles', 'watch']));

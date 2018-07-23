79ae75b184e21c994136c62d5187fa92af934037
csliva <colt.sliva@gmail.com>
2018-07-23 05:25:43 -0400

    make partial submission premium feature

--- contexts/leads/controller.js
+++ contexts/leads/controller.js
@@ -10,19 +10,19 @@ const axios = require('axios');
 
 exports.create = (req, res) => {
   //1
-  Query.findOneAndUpdate({_id :req.body.formId}, {$inc : {'count' : 1}}).then(query => {
+  Query.findOne({_id :req.body.formId}).then(query => {
     //2
     User.findById(query.user).then(user => {
-
-      var location = new Promise(function(resolve, reject) {
-        if(user.subscribed){
-          axios.get('https://ipinfo.io/'+ req.body.ip +'/geo?token=c22ea3559ce1aa').then(geo => {
-            resolve(geo.data.city + ", " + geo.data.region);
-          }).catch(error => reject(error));
-        } else {
-          resolve("")
-        }
-      });
+      if (user.subscribed || req.body.contact){
+        var location = new Promise(function(resolve, reject) {
+          if(user.subscribed){
+            axios.get('https://ipinfo.io/'+ req.body.ip +'/geo?token=c22ea3559ce1aa').then(geo => {
+              resolve(geo.data.city + ", " + geo.data.region);
+            }).catch(error => reject(error));
+          } else {
+            resolve("")
+          }
+        });
 
       location.then(geo => {
         // Create a Submission
@@ -37,14 +37,16 @@ exports.create = (req, res) => {
         // Save Submission in the database
         lead.save()
         .then(data => {
-            res.send(data);
+            Query.findOneAndUpdate({_id :req.body.formId}, {$inc : {'count' : 1}}).then(() => {
+              res.send(data);
+            })
         }).catch(err => {
             res.status(500).send({
                 message: err.message || "Some error occurred while creating the Submission."
             });
         });
       })
-
+    }
     })
   })
 }
